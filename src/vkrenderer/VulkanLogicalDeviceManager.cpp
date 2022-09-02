c312d8c414d93ef11e5d5ed67253653338e0618e
P-S-Y-S-U <mohan.gaming78@gmail.com>
2022-09-02 23:28:47 +0530

    create logical device returns a pointer to logical device

--- src/vkrenderer/VulkanLogicalDeviceManager.cpp
+++ src/vkrenderer/VulkanLogicalDeviceManager.cpp
@@ -10,13 +10,16 @@ namespace vkrender
         destroyAllLogicalDevices();
     }
 
-    void VulkanLogicalDeviceManager::createLogicalDevice( VulkanPhysicalDevice* pPhysicalDevice, VulkanSurface* pSurface )
+    VulkanLogicalDevice* VulkanLogicalDeviceManager::createLogicalDevice( VulkanPhysicalDevice* pPhysicalDevice, VulkanSurface* pSurface )
     {
         LogicalDevicePtr upLogicalDevice = std::make_unique<VulkanLogicalDevice>( pPhysicalDevice, pSurface );
 
         upLogicalDevice->createLogicalDevice();
 
+        VulkanLogicalDevice* pLogicalDevice = upLogicalDevice.get();
         m_logicalDeviceMap[pPhysicalDevice].push_back( std::move( upLogicalDevice ) );
+
+        return pLogicalDevice;
     }
 
     void VulkanLogicalDeviceManager::destroyAllLogicalDevices()
