c1414dc46f12d6d83fa3ff77d5cbd1a5e132ded7
P-S-Y-S-U <mohan.gaming78@gmail.com>
2022-09-05 20:03:10 +0530

    added file logger

--- test/vulkan/vulkan_logical_device.cpp
+++ test/vulkan/vulkan_logical_device.cpp
@@ -5,12 +5,27 @@
 #include "vkrenderer/VulkanObjectCreateInfoFactory.h"
 #include "vkrenderer/VulkanSurface.h"
 #include "window/window.h"
+#include "utilities/VulkanLogger.h"
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 #include <iostream>
 
 int main(int argc, const char* argv[])
 {
-    vkrender::Window window{};
+    spdlog::sink_ptr consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    spdlog::sink_ptr fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>( "ValidationLayer.log" );
+
+    std::initializer_list<spdlog::sink_ptr> logSinks{
+        consoleSink, fileSink
+    };
+
+    utils::VulkanValidationLayerLogger::createInstance( logSinks );
+    utils::VulkanValidationLayerLogger::getSingletonPtr()->getLogger()->set_level( spdlog::level::debug );
+
+    vkrender::Window window{};;
     vkrender::VulkanInstance instance{ "LogicalDeviceTest" };
     vkrender::VulkanDebugMessenger debugMessenger{};
 
