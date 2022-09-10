61a09202742526a639215c0c65e404bdff31d1de
P-S-Y-S-U <mohan.gaming78@gmail.com>
2022-09-10 18:31:51 +0530

    added media directory amd 2 shaders for triangle, added shader compilation steps

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,9 @@
+from json import tool
+from re import sub
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
+from os import path
+import glob
 
 class VulkanRendererConan(ConanFile):
     name = "VulkanRenderer"
@@ -17,12 +21,71 @@ class VulkanRendererConan(ConanFile):
     ] 
 
     @property
+    def _vulkan_sdk(self):
+        return tools.get_env('VULKAN_SDK')
+    
+    @property
+    def _vulkan_bin_path(self):
+        return path.join( self._vulkan_sdk, "Bin" )
+
+    @property
+    def _vulkan_shader_compiler(self):
+        return path.join( self._vulkan_bin_path, "glslc.exe" )
+
+    @property
+    def _project_media_dir(self):
+        return path.join( self.source_folder, "media" )
+    
+    @property
+    def _shaders_dir(self):
+        return path.join( self._project_media_dir, "shaders" )
+
+    @property
+    def _project_build_dir(self):
+        return path.join( self.build_folder, f"build-{self.settings.build_type}")
+    @property
+    def _project_bin_dir(self):
+        return path.join( self._project_build_dir, "bin" )
+
+    @property
     def _is_msvsc(self):
         return self.settings.os == 'Windows'
     
     @property
     def _is_linux(self):
         return self.settings.os == 'linux'
+
+    def compile_shaders(self):
+        self.output.info("compiling Shaders")
+
+        shaders = glob.glob( f"{self._shaders_dir}/*.vert" )
+
+        for shaderpath in shaders:
+            filename = shaderpath[ shaderpath.rfind("\\") + 1 : shaderpath.rfind(".") ]
+            args = [
+                self._vulkan_shader_compiler,
+                shaderpath,
+                "-o", f"{self._project_bin_dir}\\{filename}Vert.spv"
+            ]
+            cmd = " ".join(args)
+            self.output.info(f"Compiling {shaderpath}")
+            self.output.info(cmd)
+            self.run(cmd)
+        
+        shaders = glob.glob( f"{self._shaders_dir}/*.frag" )
+
+        for shaderpath in shaders:
+            filename = shaderpath[ shaderpath.rfind("\\") + 1 : shaderpath.rfind(".") ]
+            args = [
+                self._vulkan_shader_compiler,
+                shaderpath,
+                "-o", f"{self._project_bin_dir}\\{filename}Frag.spv"
+            ]
+            cmd = " ".join(args)
+            self.output.info(f"Compiling {shaderpath}")
+            self.output.info(cmd)
+            self.run(cmd)
+        
 
     def configure_cmake(self):
         cmake = CMake(self)
@@ -31,12 +94,13 @@ class VulkanRendererConan(ConanFile):
         cmake.definitions['BUILD_SHARED_LIBS'] = 'ON' if self.options.shared == True else 'OFF'
         cmake.definitions["CONAN_INSTALL_INFO"] = self.install_folder
         
-        cmake.configure(source_folder=self.source_folder, build_folder=f'{self.build_folder}/build-{self.settings.build_type}' )
+        cmake.configure(source_folder=self.source_folder, build_folder=self._project_build_dir )
         return cmake
 
     def build(self):
         cmake = self.configure_cmake()
         cmake.build()
+        self.compile_shaders()
 
     def package(self):
         cmake = self.configure_cmake()
