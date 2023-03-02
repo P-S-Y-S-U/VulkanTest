b2e946f7eb0ed771300fb4a86863b3d66934fd19
P-S-Y-S-U <mohan.gaming78@gmail.com>
2023-03-02 20:53:17 +0530

    vertex attribute and binding descriptions done

--- include/graphics/Vertex.hpp
+++ include/graphics/Vertex.hpp
@@ -2,11 +2,38 @@
 #define GRAPHICS_VERTEX_HPP
 
 #include <glm/glm.hpp>
+#include <vulkan/vulkan.hpp>
 
 struct vertex
 {
     glm::vec2 pos;
     glm::vec3 color;
+
+    static vk::VertexInputBindingDescription getBindingDescription(){
+        vk::VertexInputBindingDescription bindingDescription{};
+
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(vertex);
+        bindingDescription.inputRate = vk::VertexInputRate::eVertex;    
+
+        return bindingDescription;
+    }
+
+    static std::array<vk::VertexInputAttributeDescription, 2> getAttributeDescriptions(){
+        std::array<vk::VertexInputAttributeDescription, 2> vertexAttributes;
+
+        vertexAttributes[0].binding = 0;
+        vertexAttributes[0].location = 0;
+        vertexAttributes[0].format = vk::Format::eR32G32Sfloat;
+        vertexAttributes[0].offset = offsetof( vertex, pos );
+
+        vertexAttributes[1].binding = 0;
+        vertexAttributes[1].location = 1;
+        vertexAttributes[1].format = vk::Format::eR32G32B32Sfloat;
+        vertexAttributes[1].offset = offsetof( vertex, color );
+
+        return vertexAttributes;
+    };
 };
 
 #endif 
